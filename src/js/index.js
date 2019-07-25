be9cc510169a6ccb93d1033948e79762ae2b2bb1
yesox126 <yesox126@gmail.com>
2019-07-25 10:49:41 -0500

    Added babelrc, react on webpack config preset plugins and auto-reload.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
-import '../css/index.css'
+import React from 'react';
+import {render} from 'react-dom';
+import '../css/index.css';
+import App from './components/app.js'
 
+render(<App/>, document.getElementById('appContainer'))
+
+// if (module.hot) {
+//     module.hot.accept('./components/app.js');
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const devServer = require('webpack-dev-server');
 
 module.exports = {
 	entry: path.resolve(__dirname, './src/js/index.js'),
@@ -18,12 +19,21 @@ module.exports = {
                   },
                   'css-loader'
               ]
-          }
+		  },
+		  {
+			  test:/\.js$/,
+			  use:'babel-loader',
+			  exclude: /node_modules/
+		  }
         ]
     }, 
     plugins: [
       new MiniCSSExtractPlugin({
         filename:'[name].css'
-      })
-  ]
+	  }),
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname,'dist'),
+		hot: true
+	}  
 } 
