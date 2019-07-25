fd4b926f2c44bff9851633501e31a6029105e996
yesox126 <yesox126@gmail.com>
2019-07-25 08:58:38 -0500

    Installed webpack with plugins and loaders for CSS extraction

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,19 +81,31 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/css/index.css":
+/*!***************************!*\
+  !*** ./src/css/index.css ***!
+  \***************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("console.log(\"Hello world\")\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/css/index.css?");
+
+/***/ }),
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/index.css */ \"./src/css/index.css\");\n/* harmony import */ var _css_index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\n\n\n//# sourceURL=webpack:///./src/js/index.js?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
-    output:{
+	entry: path.resolve(__dirname, './src/js/index.js'),
+  	mode: 'development',
+  		output:{
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js' 
-    }
-}
+    },
+    module: {
+      rules:[
+          {
+             test:/\.css$/,
+              use: [
+                  {
+                    loader: MiniCSSExtractPlugin.loader
+                  },
+                  'css-loader'
+              ]
+          }
+        ]
+    }, 
+    plugins: [
+      new MiniCSSExtractPlugin({
+        filename:'[name].css'
+      })
+  ]
+} 
